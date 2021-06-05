7b7d594116407ad033177e0f2df6ed9f9f3a1206
Richard South <southy98@gmail.com>
2021-06-05 15:47:26 +0100

    rust-lang timeserver - added 2 thread sending time

--- rust-lang/timeserver/src/main.rs
+++ rust-lang/timeserver/src/main.rs
@@ -1,24 +1,57 @@
+use std::io::stdin;
+use std::thread;
+use std::time::Duration;
 use std::{
-    io::{Error, Read},
+    io::{Error, Read, Write},
     net::{Ipv4Addr, SocketAddrV4, TcpListener},
 };
 
 fn main() -> Result<(), Error> {
-    println!("Hello, world!");
+    // first thread - start up a TcpListener
+    thread::spawn(|| {
+        let socket = SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 9123);
+        let (mut stream, socket_addr) = match TcpListener::bind(socket) {
+            Ok(l) => l.accept().unwrap(),
+            _ => panic!("Failed to bind"),
+        };
 
-    let socket = SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 9123);
-    let (mut stream, socket_addr) = match TcpListener::bind(socket) {
-        Ok(l) => l.accept().unwrap(),
-        _ => panic!("Failed to bind"),
-    };
-
-    let mut buf: [u8; 100] = [0; 100];
-    match stream.read(&mut buf) {
-        Ok(bytes_read) => {
-            println!("Read {} bytes from {:?}", bytes_read, socket_addr.ip());
+        loop {
+            let mut buf: [u8; 100] = [0; 100];
+            match stream.read(&mut buf) {
+                Ok(bytes_read) => {
+                    let output = std::str::from_utf8(&buf).unwrap().to_string();
+                    println!(
+                        "{:?} Read {} bytes from {:?}, data: {}",
+                        thread::current().name(),
+                        bytes_read,
+                        socket_addr.ip(),
+                        output
+                    );
+                }
+                Err(_) => panic!(),
+            }
         }
-        Err(_) => panic!(),
-    }
+    });
 
+    thread::sleep(Duration::from_millis(500));
+
+    // second thread - client (TcpStream)
+    thread::spawn(|| {
+        let socket = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 9123);
+        let mut s = match std::net::TcpStream::connect(socket) {
+            Ok(l) => l,
+            Err(_) => todo!(),
+        };
+
+        loop {
+            thread::sleep(Duration::from_millis(500));
+            
+            let time = time::OffsetDateTime::now_utc().format("%d/%m/%Y %T");
+            println!("Publishing: {}", time);
+            s.write(time.as_bytes()).unwrap();
+        }
+    });
+
+    let _ = stdin().read(&mut [0u8]).unwrap();
     Ok(())
 }
